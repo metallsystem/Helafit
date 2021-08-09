51cd78096b581b27924743c0c359c4140b711c84
metallsystem <syava321@gmail.com>
2021-08-09 15:00:09 +0300

    main, benefits, products pages added

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -2,9 +2,9 @@ import { dest, series, src, watch } from "gulp";
 import changed from "gulp-changed";
 import gulpIf from "gulp-if";
 import imagemin from "gulp-imagemin";
-import rename from "gulp-rename";
+// import rename from "gulp-rename";
 import imageminPngquant from "imagemin-pngquant";
-import imageminWebp from "imagemin-webp";
+// import imageminWebp from "imagemin-webp";
 import config from "../config";
 
 const copyImages = () =>
@@ -22,18 +22,18 @@ const copyImages = () =>
     )
     .pipe(dest(config.dest.images));
 
-const convertImagesToWebp = () =>
-  src(`${config.src.images}/**/*.{jpg,png}`)
-    .pipe(changed(config.dest.images, { extension: ".webp" }))
-    .pipe(imagemin([imageminWebp({ quality: 75 })]))
-    .pipe(
-      rename({
-        extname: ".webp",
-      })
-    )
-    .pipe(dest(config.dest.images));
+// const convertImagesToWebp = () =>
+//   src(`${config.src.images}/**/*.{jpg,png}`)
+//     .pipe(changed(config.dest.images, { extension: ".webp" }))
+//     .pipe(imagemin([imageminWebp({ quality: 75 })]))
+//     .pipe(
+//       rename({
+//         extname: ".webp",
+//       })
+//     )
+//     .pipe(dest(config.dest.images));
 
-export const imagesBuild = series(copyImages, convertImagesToWebp);
+export const imagesBuild = series(copyImages);
 
 export const imagesWatch = () =>
   watch(`${config.src.images}/**/*`, imagesBuild);
--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -1,5 +1,6 @@
 import { dest, src, watch } from "gulp";
-import sass from "gulp-sass";
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 import plumber from "gulp-plumber";
 import autoprefixer from "gulp-autoprefixer";
 import gcmq from "gulp-group-css-media-queries";
@@ -9,11 +10,13 @@ import sassGlob from "gulp-sass-glob";
 import gulpIf from "gulp-if";
 import config from "../config";
 
+const sass = gulpSass( dartSass );
+
 export const sassBuild = () =>
   src(`${config.src.sass}/main.scss`, { sourcemaps: config.isDev })
     .pipe(plumber())
     .pipe(sassGlob())
-    .pipe(sass())
+    .pipe(sass().on('error', sass.logError))
     .pipe(gcmq())
     .pipe(autoprefixer())
     .pipe(gulpIf(config.isProd, cleanCSS({ level: 2 })))
